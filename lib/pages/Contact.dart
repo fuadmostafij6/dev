13c0ba4cd153054ad38e30dbeb6dbf3dc1546f5e
fuadmostfij6 <fuadmostfij4@gmail.com>
2022-09-15 22:17:21 +0600

    contact

--- lib/pages/Contact.dart
+++ lib/pages/Contact.dart
@@ -1,4 +1,5 @@
 
+import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
 
 class Contact extends StatefulWidget {
@@ -11,6 +12,17 @@ class Contact extends StatefulWidget {
 class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Contact");
+    return  ContactUs(
+      email: 'fuadmostafij6@gmail.com',
+
+      phoneNumber: '+88001798172012',
+      dividerThickness: 2,
+      website: 'https://abhishekdoshi.godaddysites.com',
+      githubUserName: 'fuadmostafij',
+      //linkedinURL: 'https://www.linkedin.com/in/abhishek-doshi-520983199/',
+      tagLine: 'Flutter Developer',
+      //instagram: 'fuadmostafij6',
+      textColor: Colors.redAccent, taglineColor: Colors.redAccent, cardColor: Colors.white60, companyColor: Colors.redAccent, companyName: '',
+    );
   }
 }
