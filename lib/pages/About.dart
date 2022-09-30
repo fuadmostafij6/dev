594f4ea70b9aaea0edfd2b1bc3588d74a8a25cc6
fuadmostfij6 <fuadmostfij4@gmail.com>
2022-09-30 15:40:31 +0600

    download

--- lib/pages/About.dart
+++ lib/pages/About.dart
@@ -1,9 +1,13 @@
 
+import 'dart:html';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 import '../colors_const.dart';
+
+enum ButtonState{init, loading, done}
 
 class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
@@ -14,17 +18,28 @@ class About extends StatefulWidget {
 
 class _AboutState extends State<About> {
 
-
+ButtonState state = ButtonState.init;
+downloadFile(url) {
+  AnchorElement anchorElement = AnchorElement(href: url);
+  anchorElement.download = "CV";
+  anchorElement.click();
+}
   @override
   void initState(){
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final isStretched = state == ButtonState.init;
+
+    final isDone = state == ButtonState.done;
     return  SingleChildScrollView(
       child:
       Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             color: ColorConst.lightWidgetColor,
@@ -43,7 +58,7 @@ class _AboutState extends State<About> {
           ),
           Container(
             color: ColorConst.lightWidgetColor,
-            height: size.height*0.31,
+            height: size.height*0.2,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -72,9 +87,8 @@ class _AboutState extends State<About> {
                           ]
                       ), ),
 
-                      SizedBox(
-                        height: 20.0,
-                      )
+                    
+
 
                     ],
                   ),
@@ -86,7 +100,62 @@ class _AboutState extends State<About> {
               ],
             ),
           ),
+          SizedBox(height: 15,),
+          Container(
+            alignment: Alignment.center,
+            child: Container(
+              width:size.width*0.2 ,
+              height: 50,
+              child:
+              isStretched?
+              OutlinedButton(
 
+                style: OutlinedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  side: BorderSide(width: 2, color: ColorConst.secondaryColor)
+                ),
+                onPressed: ()async {
+
+                setState(()=>state=ButtonState.loading);
+                print(state.name);
+                await Future.delayed(Duration(seconds: 3));
+                downloadFile("./assets/images/mobile.png");
+                setState(()=>state=ButtonState.done);
+                await Future.delayed(Duration(seconds: 3));
+                setState(()=>state=ButtonState.init);
+                print(state.name);
+
+              }, child: FittedBox (
+                child:
+                
+                Text("Download CV",
+
+
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: ColorConst.secondaryColor,
+                      letterSpacing: 1.5,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+
+              ): Container(
+                decoration:  BoxDecoration(
+                  shape: BoxShape.circle,
+                  color:isDone? Colors.green:ColorConst.secondaryColor,
+
+                ),
+                child:  Center(
+                  child:isDone? const Icon(
+                    Icons.done, size: 52,
+                    color: Colors.white,
+
+                  ):const CircularProgressIndicator(color: Colors.white,),
+                ),
+              ),
+            ),
+          )
         ],
       )
     );
